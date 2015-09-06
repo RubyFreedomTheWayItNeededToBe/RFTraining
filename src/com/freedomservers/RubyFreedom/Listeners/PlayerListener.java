99683ae2ebdec88a47e287e8ea7cc72698913e57
Tyler Wrenn <mcbeat257@gmail.com>
2015-09-05 21:42:24 -0400

    Attempt to make quizzing okay

--- src/com/freedomservers/RubyFreedom/Listeners/PlayerListener.java
+++ src/com/freedomservers/RubyFreedom/Listeners/PlayerListener.java
@@ -6,38 +6,66 @@ import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
 import me.StevenLawson.TotalFreedomMod.TFM_BanManager;
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
 import me.StevenLawson.TotalFreedomMod.TFM_UuidManager;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class PlayerListener implements Listener {
-
+    
+    public boolean cont = false;
+    public static PlayerCommandPreprocessEvent checkevent;
+    
     @EventHandler
     public void onPlayerLogin(PlayerLoginEvent event) {
         Player player = event.getPlayer();
-
-        if (!TFM_AdminList.isSuperAdmin(player)) {
-            player.kickPlayer(ChatColor.RED + "Server is currently in a training mode session.");
-            return;
-        }
+        
+        //TODO: Make a wizard for training
     }
 
     @EventHandler
     public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
         Player player = event.getPlayer();
         String cmd = event.getMessage();
-
+        PlayerListener.checkevent = event;
+        
+        if(cmd.equals("/trainme")) {
+            if (!TFM_AdminList.isSuperAdmin(player)) {
+                return;
+            }
+            else {
+                event.setCancelled(true);
+                RF_TrainingHandler.trainMe(player);
+            }
+        }
+    }
+    
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        String message = event.getMessage();
+        if(RF_TrainingHandler.inTrainingModeSession()) {
+            if (message.equalsIgnoreCase("continue")) {
+                event.setCancelled(true);
+                RF_TrainingHandler.continueMe(player);
+                return;
+            }
+            if (message.equalsIgnoreCase("quiz me")) {
+                event.setCancelled(true);
+                RF_TrainingHandler.quiz(player);
+                return;
+            }
+        }
     }
 
     @EventHandler
     public void onPlayerKick(PlayerKickEvent event) {
         Player player = event.getPlayer();
-        String message = event.getLeaveMessage();
+        String message = event.getLeaveMessage();        
         String ip = event.getPlayer().getAddress().getHostName().trim();
 
         if (message.contains("GTFO")) {
@@ -48,10 +76,17 @@ public class PlayerListener implements Listener {
                         TFM_BanManager.unbanUuid(TFM_UuidManager.getUniqueId(player));
                         TFM_BanManager.unbanIp(ip);
                         TFM_BanManager.unbanIp(TFM_Util.getFuzzyIp(ip));
-
                     }
                 }.runTaskLater(plugin, 2L * 20L);
             }
         }
+    }
+    
+    public boolean isContinuing(boolean cont) {
+        return this.cont;
+    }
+    
+    public void setContinuing(boolean cont) {
+        this.cont = cont;
     }
 }
--- src/com/freedomservers/RubyFreedom/RF_TrainingHandler.java
+++ src/com/freedomservers/RubyFreedom/RF_TrainingHandler.java
@@ -1,14 +1,47 @@
 package com.freedomservers.RubyFreedom;
 
+import com.freedomservers.RubyFreedom.Listeners.PlayerListener;
 import me.StevenLawson.TotalFreedomMod.Config.TFM_ConfigEntry;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class RF_TrainingHandler {
+    public static PlayerCommandPreprocessEvent checkevent;
+    
+    public RF_TrainingHandler() {
+        PlayerListener.checkevent = checkevent;
+    }
+    
+    
     public static boolean inTrainingModeSession() {
         if (TFM_ConfigEntry.TRAINING_SESSION.getBoolean()) {
             return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void trainMe(Player player) {
+        player.sendMessage(ChatColor.RED + "Welcome to RubyFreedom training! Type 'continue' in chat to continue.");
+    }
+
+    public static void continueMe(Player player) {
+        player.sendMessage(ChatColor.RED + "Go over all the commands by going to http://mc13.fadehost.com:28966/help");
+        player.sendMessage(ChatColor.RED + "When you are done, just type 'quiz me' in chat to start the quiz.");
+    }
+
+    public static void quiz(Player player) {
+        String cmd = checkevent.getMessage();
+        player.sendMessage("[QUIZ] Please respond to each question in chat.");
+        player.sendMessage("[1] Type in the command that is used to ban someone.");
+        if(!cmd.equalsIgnoreCase("/gtfo")) {
+            checkevent.setCancelled(true);
+            player.sendMessage(ChatColor.RED + "Incorrect! Try again!");
         }
         else {
-            return false;
+            checkevent.setCancelled(true);
+            player.sendMessage(ChatColor.GREEN + "Correct! Moving on.");
         }
     }
 }
