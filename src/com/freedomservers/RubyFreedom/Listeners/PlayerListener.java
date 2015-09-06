d9e4b69aecf7306e4b70ccf72829fe6627b709c0
Tyler Wrenn <mcbeat257@gmail.com>
2015-09-05 20:38:41 -0400

    Add a ton of crap

---
+++ src/com/freedomservers/RubyFreedom/Listeners/PlayerListener.java
@@ -0,0 +1,7 @@
+package com.freedomservers.RubyFreedom.Listeners;
+
+import org.bukkit.event.Listener;
+
+public class PlayerListener implements Listener {
+    
+}
--- src/com/freedomservers/RubyFreedom/RFTraining.java
+++ src/com/freedomservers/RubyFreedom/RFTraining.java
@@ -1,7 +1,32 @@
 package com.freedomservers.RubyFreedom;
 
+import com.freedomservers.RubyFreedom.Listeners.PlayerListener;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RFTraining extends JavaPlugin {
-    
+
+    public static RFTraining plugin;
+    public Server server;
+
+    public String version = plugin.getDescription().getVersion();
+
+    @Override
+    public void onLoad() {
+        RFTraining.plugin = this;
+    }
+
+    @Override
+    public void onEnable() {
+        final PluginManager pm = Bukkit.getPluginManager();
+        pm.registerEvents(new PlayerListener(), plugin);
+        RFLogger.info("RFTraining v" + version + "has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        RFLogger.info("RFTraining v" + version + "has been disabled!");
+    }
 }
